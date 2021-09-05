8bea74e3978bad0638113f644dfa175a777224ee
Igor Konnov <57567427+igorkonnov@users.noreply.github.com>
2021-09-05 13:35:12 +0300

    Update cem_main.py

--- machine_learn/cem_main.py
+++ machine_learn/cem_main.py
@@ -1,9 +1,9 @@
 import dash
-import dash_table
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dash_table
+from dash import  dcc
+from dash import html
 import plotly.graph_objs as go
-import dash_table
+from dash import dash_table
 import pandas as pd
 import numpy as np
 import plotly.express as px
